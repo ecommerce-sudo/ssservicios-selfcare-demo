2d6dc8814553e94a0d39ea83d2ecf038a0b553b2
ecommerce-sudo <ecommerce@ssservicios.com.ar>
2026-02-19 14:14:57 -0300

    Update QuickActionsCard.tsx

--- apps/web/app/_components/QuickActionsCard.tsx
+++ apps/web/app/_components/QuickActionsCard.tsx
@@ -81,7 +81,7 @@ function Item({
       <div
         style={{
           fontSize: 12,
-          fontWeight: 900,
+          fontWeight: 700,
           color: "#0f172a",
           opacity: 0.9,
           lineHeight: 1.15,
